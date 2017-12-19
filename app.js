69c85ea6b8d521ac88f60b0ff1605457415759c2
MatthewMcGowan <mattmcgowan1989@hotmail.com>
2017-12-19 17:37:11 +0000

    Implemented both get multiple and get single.

--- app.js
+++ app.js
@@ -10,11 +10,28 @@ var bookRouter = express.Router();
 
 bookRouter.route('/books')
     .get(function(req, res) {
-        Book.find(function(err, books) {
+        var query = {};
+
+        if(req.query.genre)
+        {
+            query.genre = req.query.genre;
+        }
+
+        Book.find(query, function(err, books) {
             if(err)
                 res.status(500).send(err);
             else
                 res.json(books);
+        });
+    });
+
+bookRouter.route('/books/:bookId')
+    .get(function(req, res) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(book);
         });
     });
 
