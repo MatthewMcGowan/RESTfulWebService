0923c6175f852eee5890d9b92d4de0b652c8b70b
MatthewMcGowan <mattmcgowan1989@hotmail.com>
2017-12-19 20:57:35 +0000

    Added post, with minor route tidy.

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
 
-bookRouter.route('/books')
-    .get(function(req, res) {
-        var query = {};
+bookRouter = require('./routes/bookRoutes')(Book);
 
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-
-        Book.find(query, function(err, books) {
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/books/:bookId')
-    .get(function(req, res) {
-        Book.findById(req.params.bookId, function (err, book) {
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-    });
-
-app.use('/api', bookRouter);
-
-app.get('/', function(req, res) {
-   res.send('Welcome to my API!');
-});
+app.use('/api/books', bookRouter);
 
 app.listen(port, function () {
     console.log('Running on PORT: ' + port);
