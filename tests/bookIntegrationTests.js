b6104245d65a08f64fbbf6452cc6922914764cb8
MatthewMcGowan <mattmcgowan1989@hotmail.com>
2017-12-22 00:13:06 +0000

    Added integration test.

--- tests/bookIntegrationTests.js
+++ tests/bookIntegrationTests.js
@@ -6,17 +6,44 @@ var should = require('should'),
     agent = request.agent(app);
 
 describe('Book CRUD Test', function () {
-    it('should allow a book to be posted and return a read and _id', function (done) {
-        var bookPost = {title: 'new Book', author: 'new Author', genre: 'Fiction'};
+    describe('when a book is POSTed', function () {
 
-        agent.post('/api/books')
-            .send(bookPost)
-            .expect(200)
-            .end(function(err, res) {
-                res.body.read.should.equal(false);
-                res.body.should.have.property('_id');
-                done();
-            })
+        it('should return read and _id fields', function (done) {
+            var postedBook = {title: 'new Book', author: 'new Author', genre: 'Fiction'};
+
+            agent.post('/api/books')
+                .send(postedBook)
+                .expect(200)
+                .end(function (err, res) {
+                    res.body.read.should.equal(false);
+                    res.body.should.have.property('_id');
+                    done();
+                })
+        });
+
+        it('should be retrievable with a GET', function (done) {
+            var postedBook = {title: 'test Book', author: 'test Author', genre: 'test Genre', read: true};
+
+            var GetBook = function (id) {
+                return agent.get('/api/books/' + id)
+                    .expect(200)
+                    .end(function (err, res) {
+                        for(var key in bookPost) {
+                            bookPost[key].should.equal(res.body[key]);
+                        }
+                    });
+            };
+
+            agent.post('/api/books')
+                .send(postedBook)
+                .expect(200)
+                .end(function (err, res) {
+                    var id = res.body._id;
+                    GetBook(id);
+                    
+                    done();
+                });
+        });
     });
 
     afterEach(function (done) {
